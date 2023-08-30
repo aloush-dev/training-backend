13a395b86e583ae94cf1d86c23a606e3b4923cac
itsaliabdallah <itsaliabdallah@gmail.com>
2023-08-30 12:56:41 +0100

    add a form to make new post requests

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const {
   postProduct,
 } = require("./controllers/products.controller");
 const cors = require("cors");
-const { getIndex, pugPostProduct } = require("./controllers/pugPages.controller");
+const {
+  getIndex,
+  pugPostProduct,
+} = require("./controllers/pugPages.controller");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.get("/", getIndex);
 
-app.get('/postproduct', pugPostProduct )
+app.get("/postproduct", pugPostProduct);
 
 app.get("/api/", (req, res) => {
   res.status(200).send({ msg: "server is running" });
